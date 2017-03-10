f7072379ccdc2bcf4cd1df03ec4f6e6490650fa9
aliciasavelly <alicia.savelly@gmail.com>
2017-03-10 13:59:01 -0800

    create greeting container

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -12,6 +12,7 @@ window.logout = logout;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+  window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={store}>
