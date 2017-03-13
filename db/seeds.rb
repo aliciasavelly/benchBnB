6f040fd9d3150f3bd95ff98e98c66ed2e6cc0f26
aliciasavelly <alicia.savelly@gmail.com>
2017-03-13 16:24:04 -0700

    create benches controller and allow for POST and GET ajax requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Bench.create!(
+  description: "bench in the mission, nice",
+  lat: 37.764674,
+  lng: -122.427309
+)
+
+Bench.create!(
+  description: "on the embarcadero",
+  lat: 37.797298,
+  lng: -122.394343
+)
+
+Bench.create!(
+  description: "nob hill is fun",
+  lat: 37.791058,
+  lng: -122.411853
+)
